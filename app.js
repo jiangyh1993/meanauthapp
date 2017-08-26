3117436c1d7063395b16748cf14802934df4c370
Unknown <158840284@qq.com>
2017-08-26 14:47:53 -0500

    auth app
    add user in database

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to database
+mongoose.connect(config.database, {
+    useMongoClient: true
+});
+
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('connected ERROR ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+//cors middleware
+app.use(cors());
+
+//body parser middleware
+app.use(bodyParser.json());
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid endpoint');
+});
+
+app.listen(port, () => {
+    console.log('Server start at port ' + port);
+});
